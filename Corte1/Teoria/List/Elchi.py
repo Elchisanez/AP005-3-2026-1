fc04fce1df3959dfc89579ad7725b4f6f0c92ef5
Elchisanez <josegregoriog600@gmail.com>
2026-03-04 15:23:38 -0500

    Update Elchi.py

--- Corte1/Teoria/List/Elchi.py
+++ Corte1/Teoria/List/Elchi.py
@@ -1,1 +1,18 @@
+#############LISTAS################
+###################################
+my_liatas = ['Rojo', 'Azul', 'Amarillo', 'Naranja',  'Verde']
+#input()
+pritn(my_lista)
+print(type(my_lista))
+print(my_lista[2])
 
+pritn(my_lista size: ", len(my_lista))
+print("my_lista[0:2])
+print(my_lista[:2])
+
+my_lista.append('Blanco')
+print(my_lista)
+
+my_lista.insert(3, 'negro')
+      pritn(my_lista)
+      
